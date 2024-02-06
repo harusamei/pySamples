83afedcffb5e3c8d7d5ccc4058ed0a448fd43de8
Yao Yao1 Meng <mengyao1@Lenovo.com>
2024-02-06 13:15:35 +0800

    build a timeprocess serviceBySanic and openAI async.

---
+++ openAI async.py
@@ -0,0 +1,64 @@
+# 使用异步方法调用openAI的GPT-3.5-turbo模型
+
+import os
+import pandas as pd
+import asyncio
+from openai import AsyncOpenAI
+
+client = AsyncOpenAI(
+    api_key="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"
+
+)
+
+# define the task of async
+async def callGPT(query): 
+    print(f'say {query} to GPT')
+    chat_completion = await client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": query,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    print(chat_completion.choices[0].message.content)
+    
+    return chat_completion.choices[0].message.content
+
+    
+async def main() -> None:
+    
+    df_answer =  pd.read_excel("doublePara.xlsx")
+    #append a new colum to df_answer
+    df_answer['GPT'] = ''
+    print(df_answer.shape)
+
+    # create a task list
+    tasks = []
+    prompt = "请对下面句子进行改写，要求用中文并且保持原意不变\n"
+    for i in range(df_answer.shape[0]):
+        query= prompt + df_answer.loc[i, "指代问题的上一轮问题"] # + "\n" + df_answer.loc[i, "指代问题"]
+        task = asyncio.create_task(callGPT(query))
+        tasks = tasks + [task]
+
+    print(len(tasks))
+    
+    # 每次只执行100个任务
+    batch_size = 100
+    for i in range(0, len(tasks), batch_size):
+        await asyncio.gather(*tasks[i:i+batch_size])
+
+    # get the result of the task
+    for i, task in enumerate(tasks):
+        
+        new_answer = task.result()
+        df_answer.loc[i, "GPT"] = new_answer
+
+    # save the dataframe to excel file
+    print(df_answer.shape)
+    df_answer.to_excel("answer.xlsx", index=False)
+
+    print("Done!")
+
+asyncio.run(main())
---
+++ serviceBySanic.py
@@ -0,0 +1,93 @@
+# 使用sanic框架，实现一个简单的时间格式规范化服务
+
+from sanic import Request, Sanic, json,text
+import asyncio
+
+from timeProcess import DateTimeProc
+from urllib.parse import unquote
+
+app = Sanic("DateNormalize")
+dp=DateTimeProc()
+
+ip_freq = {}
+tags = [
+    {"url": "/zh", "name": "规范中文时间格式"},
+    {"url": "/en", "name": "规范英文时间格式"},
+    {"url": "/home", "name": "规范中文数字格式"}
+]
+
+def isHighFreqIp(request):
+    ip_address = request.ip
+    print(f"Received request from {ip_address}")  
+
+    if ip_address in ip_freq:
+        ip_freq[ip_address] += 1
+    else:
+        ip_freq[ip_address] = 1
+
+    if ip_freq[ip_address] > 5:
+        return True
+       
+    return False
+
+async def background_task():
+    while True:
+        print("Running background task, clear ip_mem")
+        # ip 记数清零
+        for key in ip_freq:
+            ip_freq[key] = 0
+        await asyncio.sleep(5)
+
+async def callTimeProc(query, lang):
+    return dp.parseDateTime(query, lang)
+
+@app.listener('before_server_start')
+async def setup_background_task(app, loop):
+    loop.create_task(background_task())
+
+@app.listener('after_server_stop')
+async def notify_server_stopped(app, loop):
+    print(ip_freq)
+    print("Server stopped!")
+
+# 所有请求都会调用这个中间件
+@app.middleware('request')
+async def print_on_request(request):
+    # 如果请求频率过高，返回错误信息，不再处理
+    if isHighFreqIp(request):
+        return json({'message': 'Too many requests from your IP address!'})
+    print("A request is coming")
+
+@app.route("/")
+@app.route("/home")
+async def home(request : Request):
+    
+    temStr = unquote(request.query_string)
+    temVal = dp.str2val(temStr)
+    return text(f"{temVal}")
+
+# curl "http://localhost:8000/zh?2019年三月七日下午10点差五分"
+@app.route("/zh")
+async def zh_CN(request):
+    temStr = unquote(request.query_string)
+    temVal = await callTimeProc(temStr, "zh_CN")
+    return text(f"{temVal}")
+
+# curl "http://localhost:8000/en?January 2019"
+@app.route("/en")
+async def en_US(request):
+    temStr = unquote(request.query_string)
+    temVal = await callTimeProc(temStr, "en_US")
+    return text(f"{temVal}")
+
+@app.get("/help")
+async def help(request: Request):
+   
+    print(request.args)
+    return json(tags)
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=8000)
+
